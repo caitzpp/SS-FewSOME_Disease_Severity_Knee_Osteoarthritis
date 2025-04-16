6d55eb7d69f22ebb8aa67bc9ec760e9596085484
Caitlyn Zuppinger <caitlyn.zuppinger@outlook.com>
2025-04-16 13:43:23 +0200

    installed Python 3.11.8

--- setup_ssfewsome.sh
+++ setup_ssfewsome.sh
@@ -7,8 +7,23 @@
 #SBATCH --cpus-per-task=2
 #SBATCH --partition=performance
 
+PYTHON_VERSION=3.11.8
+INSTALL_DIR=$HOME/python-$PYTHON_VERSION
+
+mkdir -p $HOME/build_python
+cd $HOME/build_python
+
+wget https://www.python.org/ftp/python/$PYTHON_VERSION/Python-$PYTHON_VERSION.tgz
+tar xzf Python-$PYTHON_VERSION.tgz
+cd Python-$PYTHON_VERSION
+
+./configure --prefix=$INSTALL_DIR --enable-optimizations
+make -j4
+make install
+
 cd /home2/c.zuppinger/VT9_SSFewSOME/SS-FewSOME_Disease_Severity_Knee_Osteoarthritis
-python3 -m venv myenv
+$INSTALL_DIR/bin/python3.11 -m venv myenv
+#python3 -m venv myenv
 source myenv/bin/activate
 
 pip install --upgrade pip

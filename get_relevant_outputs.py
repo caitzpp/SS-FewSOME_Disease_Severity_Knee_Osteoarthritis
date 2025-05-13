e611ef399f86fc198c7bd5751e5de74afc1af392
Caitlyn Zuppinger <caitlyn.zuppinger@outlook.com>
2025-05-13 17:52:34 +0200

    refactor(download_data): make it easier to download data

--- get_relevant_outputs.py
+++ get_relevant_outputs.py
@@ -1,15 +1,15 @@
 import os
 import shutil
 
-
+#TODO: make sure to run it in the SS-FewSOME_Disease_xxx dir using cd
 mod_prefix= "mod_2"
 
-best_epochs = {'1001': 1390, '138647': 1190, '193': 800, '34': 1270, '44': 1290, '71530': 1250, '875688': 800, '8765': 800, '985772': 1320, '244959': 1380}
+best_epochs = {'1001': 1990, '138647': 2110, '193': 1550, '34': 2040, '44': 2070} # for stage 2 of mod_2 {'1001': 1390, '138647': 1190, '193': 800, '34': 1270, '44': 1290, '71530': 1250, '875688': 800, '8765': 800, '985772': 1320, '244959': 1380}
 outputs_path = "./ss_fewsome/outputs"
 save_path = "./ss_fewsome/downloads"
 
 folders = ['logs', 'results', 'dfs', 'models'] #'results', 'dfs', 'models', 
-stages = [ 'stage2']  #'stage_severe_pred', 'stage3' , 'ss',
+stages = [ 'stage3']  #'stage_severe_pred', 'stage3' , 'ss',
 
 if __name__=="__main__":
     os.makedirs(save_path, exist_ok=True)

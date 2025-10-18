f34b1cc8e4edfdc2af36b9e7486688170998465c
Caitlyn Zuppinger <caitlyn.zuppinger@outlook.com>
2025-10-18 23:29:05 +0200

    feat(wandb_sweeps)

--- scripts/run_inference.sh
+++ scripts/run_inference.sh
@@ -20,18 +20,22 @@ singularity exec --nv --no-home \
   -B "$DATA_FOLDER":/data \
   -B "$HOME/VT9_SSFewSOME":/home2/c.zuppinger/VT9_SSFewSOME \
   "$SIF_FILENAME" python /workspace/"$FILENAME" \
-  --data_path "/data" \
+  --data_path "/data/600x600_imgs" \
   --dir_path /home2/c.zuppinger/VT9_SSFewSOME/SS-FewSOME_Disease_Severity_Knee_Osteoarthritis/ss_fewsome \
   --train_ids_path /home2/c.zuppinger/VT9_SSFewSOME/SS-FewSOME_Disease_Severity_Knee_Osteoarthritis/meta2/ \
   --device cuda \
   --eval_epoch 1 \
   --task all \
   --save_models 0 \
-  --model_name mod_st \
+  --model_name mod_smallimg2 \
   --use_wandb 0 \
   --train_ss 1 \
   --stage2 0 \
   --stage3 0 \
   --stage_severe_pred 0 \
-  --lr 1e-06 \
+  --lr 0.0005\
   --bs 1 \
+  --weight_decay 0.0001 \
+  --beta1 0.85 \
+  --beta2 0.999 \
+  --eps 1e-08 \
